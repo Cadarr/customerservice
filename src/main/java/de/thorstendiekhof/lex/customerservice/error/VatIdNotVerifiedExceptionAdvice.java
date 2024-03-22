6a92f2cd5159bb138e30d7027e250bc12f630e14
Thorsten Diekhof <kontakt@thorsten-diekhof.de>
2024-03-22 06:04:42 +0100

    feat: default openAPI Documentation

--- src/main/java/de/thorstendiekhof/lex/customerservice/error/VatIdNotVerifiedExceptionAdvice.java
+++ src/main/java/de/thorstendiekhof/lex/customerservice/error/VatIdNotVerifiedExceptionAdvice.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -15,6 +16,7 @@ import java.util.HashMap;
 public class VatIdNotVerifiedExceptionAdvice {
 
     @ExceptionHandler(VatIdNotVerifiedException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<Object> handleValidationExceptions(
             VatIdNotVerifiedException ex, WebRequest request) {
 
